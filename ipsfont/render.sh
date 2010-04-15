f116a06ad5483f7a32701b1f87f9ff01d8a044fa
Edward Fox <edyfox@gmail.com>
2010-04-15 09:18:59 +0000

    Fixed the mistakes in the copyright statements

--- ipsfont/render.sh
+++ ipsfont/render.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-#  Copyright 2008 Google Inc.
+# Copyright 2010 Edward Leap Fox (edyfox@gmail.com).
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
